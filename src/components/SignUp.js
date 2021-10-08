d6fcdde1b8794fd27d1ab3071835c062bb4876bb
Hatice <haticekaraty@gmail.com>
2021-10-07 20:05:59 -0400

    add eventListeners to controlled form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,13 +6,28 @@ class SignUp extends React.Component{
         email: "",
         password: ""
     }
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // need to submit the user data to backend 
+        console.log("submit: ", this.state)
+    }
+
     render(){
         return(
             <div>
-                <form>
-                    <input type="text" name="username" value={this.state.username} placeholder="username"/>
-                    <input type="text" name="email" value={this.state.email} placeholder="email"/>
-                    <input type="text" name="password" value={this.state.password} placeholder="password"/>
+                <h1>Signup</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleChange} type="text" name="username" placeholder="username"/><br />
+                    <input onChange={this.handleChange} type="text" name="email" placeholder="email"/><br />
+                    <input onChange={this.handleChange} type="text" name="password" placeholder="password"/><br />
+                    <input type="submit" value="SignUp" />
                 </form>
             </div>
         )
