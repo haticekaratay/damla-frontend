9e47bdfa7a0001084f388f10e7fbd1a1d32cdbe7
Hatice <haticekaraty@gmail.com>
2021-10-09 20:39:29 -0400

    create store in index.js and pass it to components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-   <Router>
-      <App />
-    </Router>,
+    <Provider store={ store }>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
   document.getElementById('root')
 );
 
