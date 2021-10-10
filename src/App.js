9f6cff76a3c76d601a0903c7c25e353d30523a17
Hatice <haticekaraty@gmail.com>
2021-10-10 17:23:21 -0400

    call autologin via connect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import React from "react";
 import {Route} from "react-router-dom"
+import { connect } from 'react-redux'
+import {autoLogin} from "./actions/userActions"
+
 import SignUp from "./components/SignUp";
 import Home from "./components/Home"
-import Login from './components/Login';
+import Login from "./components/Login";
+import IncomeContainer from "./components/incomes/incomeContainer";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/login" component={Login} />
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    this.props.autoLogin()
+    return (
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/mybudget" component={IncomeContainer} />
+      </div>
+    );
+  }
 }
-
-export default App;
+export default connect(null,{autoLogin})(App);
