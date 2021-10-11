3868afd49195abb15b2a2c90dc787c4995c066f9
Hatice <haticekaraty@gmail.com>
2021-10-10 22:02:08 -0400

    Fetch all incomes for current user and post new

---
+++ src/actions/incomeActions.js
@@ -0,0 +1,52 @@
+export const userIncomes = () => {
+    return (dispatch) => {
+        const token = localStorage.token
+        fetch("http://localhost:3001/incomes",{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept" : "application/json",
+                'Authorization': `${ token }`
+            }
+        })
+        .then(resp => {
+            // console.log("resp from autologin: ", resp.json())
+            return resp.json()
+        })
+        .then(incomeData => {
+            if(incomeData.error){
+                console.log("response from autologin error:")
+            }else{
+            console.log("in incomeData:", incomeData)
+            dispatch({type: "GET_USER_INCOMES", incomes: incomeData})
+            }
+        }).catch(console.log)
+    }
+}
+
+export const addIncome = () => {
+    return (dispatch) => {
+        const token = localStorage.token
+        fetch("http://localhost:3001/incomes",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept" : "application/json",
+                'Authorization': `${ token }`
+            }
+        })
+        .then(resp => {
+            console.log("resp from addIncome: ", resp.json())
+            return resp.json()
+        })
+        .then(incomeData => {
+            if(incomeData.error){
+                console.log("response from autologin error:")
+            }else{
+            console.log("in incomeData:", incomeData)
+            dispatch({type: "ADD_INCOME", income: incomeData})
+            }
+        }).catch(console.log)
+    }
+}
+
