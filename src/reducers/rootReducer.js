1be2620931a7d728d8cba88c5c0c3a9086f81c69
Hatice <haticekaraty@gmail.com>
2021-10-13 18:56:39 -0400

    combine all reducers to one root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,25 +1,12 @@
-const initialState = {
-    currentUser: null,
-    expenses: [],
-    incomes: []
-}
-const userReducer = (state = initialState, action) => {
-    switch(action.type){
-        case "CREATE_USER":
-            return {...state, currentUser: action.user}
-        case "LOGIN_USER":
-            console.log("Dispatch user: ",action.user)
-            return {...state,currentUser: action.user}
-        case "GET_USER_INCOMES":
-            return {...state, incomes: action.incomes}
-        case "ADD_INCOME":
-            console.log("add Income:", action.income)
-            return {...state, incomes: [...state.incomes,action.income]}
-        case "GET_USER_EXPENSES":
-            return {...state, expenses: action.expenses}
-        default:
-            return state
-    }
-}
+import { combineReducers } from 'redux';
+import incomeReducer from "./incomeReducer";
+import expenseReducer from "./expenseReducer";
+import userReducer from "./userReducer";
 
-export default userReducer
+const rootReducer = combineReducers({
+    users: userReducer,
+    expenses: expenseReducer,
+    incomes: incomeReducer
+})
+
+export default rootReducer;
