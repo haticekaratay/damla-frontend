4e3819bcb8464d6ce6ab58876ee543eaeeb868d0
Hatice <haticekaraty@gmail.com>
2021-10-13 22:54:18 -0400

    add cards with tabs to the incomes container

--- src/components/incomes/incomesContainer.js
+++ src/components/incomes/incomesContainer.js
@@ -1,13 +1,26 @@
 import React from "react";
 import IncomeInput from "./incomeInput";
 import Incomes from "./incomes";
+import {Card} from "react-bootstrap"
+import { Tab, Tabs } from "react-bootstrap";
 
 const incomesContainer = (props) => {
     return(
         <div>
-            <IncomeInput />
-            <Incomes />
+            <Card >
+                <Card.Header>Your Income</Card.Header>
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                <Tab eventKey="home" title="Incomes">
+                    <Incomes/>
+                </Tab>
+                <Tab eventKey="incomes" title="Add Income">
+                    <IncomeInput />
+                </Tab>
+            </Tabs>
+                
+            </Card>
         </div>
     )
 }
 export default incomesContainer;
+
