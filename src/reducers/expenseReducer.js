38e8859dda76ea1c1c1572ae28b0e403536430a7
Hatice <haticekaraty@gmail.com>
2021-10-17 14:35:47 -0400

    update expense reducer to dispatch add expense

--- src/reducers/expenseReducer.js
+++ src/reducers/expenseReducer.js
@@ -5,6 +5,8 @@ const expenseReducer = (state = initialState, action) => {
     switch(action.type){
         case "GET_USER_EXPENSES":
             return {...state, expenses: action.expenses}
+        case "ADD_EXPENSE":
+            return{...state, expenses: [...state.expenses,action.expense]}
         default:
             return state
     }
