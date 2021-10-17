3722197fbac19a3e3450f06e05098aeb5549ff0b
Hatice <haticekaraty@gmail.com>
2021-10-17 14:34:02 -0400

    add edit icons to each expense

--- src/components/expenses/expense.js
+++ src/components/expenses/expense.js
@@ -1,32 +1,40 @@
-import React from "react";
-import { Bar } from 'react-chartjs-2';
-import { ProgressBar } from "react-bootstrap";
+import React from "react"
+import { ProgressBar, Button } from "react-bootstrap"
+import { GrFormEdit } from "react-icons/gr"
+import { GoPencil } from "react-icons/go"
+
+import { connect } from "react-redux"
 
 const Expense = (props) => {
-//    debugger
-    const now = parseInt(props.expense.amount)/parseInt(props.expense.budget)*100
-    console.log("parseInt(props.expense.amount):", parseInt(props.expense.amount))
-    console.log("parseInt(props.expense.budget):",parseInt(props.expense.budget))
-    console.log("now:",now)
-    return(
-        // <div style={{height:"500px", width:"400px"}}>
-        <div style={{ width:"400px"}}>
-            <li>{props.expense.name}------{props.expense.amount}---{props.expense.budget}</li>
-            <ProgressBar>
-                <ProgressBar  striped variant="bar-graph" now={props.expense.amount} label={`$${props.expense.amount}`}/>
-                <ProgressBar  striped variant="base" now={props.expense.budget-props.expense.amount} label={`$${props.expense.budget-props.expense.amount}`}/>
-            </ProgressBar>
 
-            {/* <ProgressBar  variant="bar-graph" max={props.expense.budget} now = {props.expense.amount}></ProgressBar> */}
-            
+    const handleClick= () => {
+        // allow user to only update amount
+        
+    }
+
+    const now = parseInt((props.expense.amount/props.expense.budget)*100)
+
+    return(
+        <div className="expence-container">
+            {props.expense.name}------{props.expense.amount}---{props.expense.budget}
+            <div>
+                <ProgressBar style={{height:"20%"}}>
+                    <ProgressBar  striped variant="bar-graph" now={now}  label={`$${props.expense.amount}`}/>
+                    <ProgressBar  striped variant="base" now={(props.expense.budget-props.expense.amount)} max={props.expense.budget}label={`$${props.expense.budget-props.expense.amount}`}/>
+                </ProgressBar>
+            </div>
+            <div>
+                <Button size="sm" onclick={()=>handleClick()}><GrFormEdit/></Button>
+                <Button size="sm "><GoPencil/></Button>
+            </div>
         </div>
+    
     )
 }
+const mapStateToProps = (state) => {
+   return {
+        expenses: state.expenses.expenses
+   } 
+}
 
-export default Expense;
-
-// const now = 60;
-
-// const progressInstance = <ProgressBar now={now} label={`${now}%`} />;
-
-// render(progressInstance);
+export default connect(mapStateToProps)(Expense);
