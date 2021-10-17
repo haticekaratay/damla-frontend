e0419d9d61a662538ec469f3d68e17792d0acc8e
Hatice <haticekaraty@gmail.com>
2021-10-17 04:19:32 -0400

    update navbar alignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,53 @@
 import React from "react"
-import { Nav, Navbar,NavItem} from "react-bootstrap"
+import { Nav, Navbar,NavItem, Container} from "react-bootstrap"
 import { connect } from "react-redux"
 import {logout} from "../actions/userActions"
+import {withRouter} from "react-router"
 
 const NavBar = (props) => {
 
+    console.log("withRouter props:", props)
     const handleLogout = () => {
         console.log("logout", props)
         localStorage.removeItem("token")
         props.logout()
+        props.history.push("/")
     }
+    
     return(
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src="/public/damla.png"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />
-                    {' '} DAMLA</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/mybudget">My Budget</Nav.Link>
+
+        <Navbar variant="dark" className="navbar-custom">
+            <Container>
+                <Navbar.Brand href="/">Home</Navbar.Brand>
+                <Nav.Link href="/mybudget" style={{ color: "white" }}>My Budget</Nav.Link>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
                         <Nav.Link onClick={()=>handleLogout()}>Logout</Nav.Link>
-                        <NavItem onClick={()=>handleLogout()}>Logout2</NavItem>
-                    </Nav>
-                </Navbar>
-        </div>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+
+
+
+
+
+
+        // <div>
+        //     this is test
+        //     <Navbar bg="dark" variant="dark">
+        //             <Nav className="justify-content-end" style={{ width: "100%" }}>
+        //                 <Nav.Link href="/">Home</Nav.Link>
+        //                 <Nav.Link href="/mybudget">My Budget</Nav.Link>
+        //                 <div ><Nav.Link  onClick={()=>handleLogout()}>Logout</Nav.Link></div>
+                        
+        //             </Nav>
+        //         </Navbar>
+        // </div>
     )
 }
 
 
-export default connect(null,{logout})(NavBar);
+export default withRouter(connect(null,{logout})(NavBar));
