81abb2665c08b37f641392dc58a185355df1fa9e
Hatice <haticekaraty@gmail.com>
2021-10-20 23:36:34 -0400

    update signup and login styling with bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React from "react"
-import {connect} from "react-redux"
-import {loginUser} from "../actions/userActions"
-import {Row,Col, Form,Button, Card, Image,Container} from "react-bootstrap"
+import { connect } from "react-redux"
+import { loginUser } from "../actions/userActions"
+import { Row,Col, Form,Button, Card, Image,Container } from "react-bootstrap"
 import damla from "../damla.png"
+import { Link } from "react-router-dom"
 
 
 class Login extends React.Component{
@@ -14,72 +15,48 @@ class Login extends React.Component{
         this.setState({
             [e.target.name]: e.target.value
         })
-        console.log(this.state)
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        // need to submit the user data to backend 
-        console.log("submit: ", this.state)
         this.props.loginUser(this.state,this.props.history)
     }
 
-     styles = {
-        center: {
-          marginLeft: "auto",
-          marginRight: "auto"
-        }
-    }
-
-    render(){
+    render(){        
         return(
-            <>
-            {/* <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} type="text" name="username" placeholder="username"/><br />
-                    <input onChange={this.handleChange} type="text" name="password" placeholder="password"/><br />
-                    <Button variant="primary" type="submit">
-                        LOGIN
-                    </Button>
-                </form>
-            </div> */}
-             
-            <div className={this.styles.center} >
-            <Container>
-                <Card className=" align-items-center justify-content-md-center">
-
-                    <Row xs={2} className="align-items-center justify-content-md-center">
-                        <Col  >
+            <Container className="login-container">
+                <Card >
+                    <Row  className=" d-flex align-items-center justify-content-center">
+                        <Col>
                             <Image style={{width: '100%', height: '100%'}} src={damla} />
                         </Col>
-                        <Col  >
-                        <Form onSubmit={this.handleSubmit} className="form-signin">
-                            <Form.Label>DAMLA</Form.Label>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control onChange={this.handleChange} type="username" name="username" placeholder="Enter username" />
-                            </Form.Group>
+                        <Col>
+                            <Form onSubmit={this.handleSubmit} className="form-signin">
+                                <Form.Label>DAMLA</Form.Label>
+                                <Form.Group >
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control onChange={this.handleChange} type="username" name="username" placeholder="Enter username" />
+                                </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control  onChange={this.handleChange} type="password" name="password" placeholder="Password" />
-                            </Form.Group>
-                            <Button variant="secondary" type="submit">
-                                LOGIN
-                            </Button>
+                                <Form.Group >
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control  onChange={this.handleChange} type="password" name="password" placeholder="Password" />
+                                </Form.Group>
+                                <br></br>
+                                <Button   Button variant="secondary" type="submit">
+                                    SIGN IN
+                                </Button>
                         </Form>
+                        <br></br>
+                            <Link to="/signup" style={{ color: "black" }}>
+                                SIGN UP
+                            </Link> 
                         </Col>
                     </Row>
                 </Card>
             </Container> 
-            </div>
-
-            </>
-        
-        )
+        )  
     }
-
 }
 
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { connect } from 'react-redux'
-import {createUser} from "../actions/userActions"
-
+import { createUser} from "../actions/userActions"
+import { Row,Col,Form,Button,Card,Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
 class SignUp extends React.Component{
     state={
         username: "",
@@ -18,23 +19,47 @@ class SignUp extends React.Component{
 
     handleSubmit = (e) => {
         e.preventDefault();
-        // need to submit the user data to backend 
-        //console.log("submit: ", this.state)
-        console.log(this.props) //props has the history so pass it to create user to redirect
         this.props.createUser(this.state, this.props.history)
     }
 
     render(){
         return(
-            <div>
-                <h1>Signup</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} type="text" name="username" placeholder="username"/><br />
-                    <input onChange={this.handleChange} type="text" name="email" placeholder="email"/><br />
-                    <input onChange={this.handleChange} type="text" name="password" placeholder="password"/><br />
-                    <input type="submit" value="SignUp" />
-                </form>
-            </div>
+
+            <Container className="signup-container">
+                <Card className="signup-card">
+                    <Row>
+                        <Col>
+                            <Form onSubmit={this.handleSubmit}>
+                                <h3>SIGN UP</h3>
+                                <hr></hr>
+                                <Form.Group >
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control onChange={this.handleChange} type="username" name="username" placeholder="Enter username" />
+                                </Form.Group>
+
+                                <Form.Group >
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control onChange={this.handleChange} type="email" name="email" placeholder="Enter email" />
+                                </Form.Group>
+
+                                <Form.Group >
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control  onChange={this.handleChange} type="password" name="password" placeholder="Password" />
+                                </Form.Group>
+                                <br></br>
+                                <Button   Button variant="secondary" type="submit">
+                                    SIGN UP
+                                </Button>
+                        </Form>
+                        <br></br>
+                        If you already signed up.
+                            <Link to="/login" style={{ color: "black" }}>
+                                LOG IN
+                            </Link> 
+                        </Col>
+                    </Row>
+                </Card>
+            </Container> 
         )
     }
 }
