a0b66eef0f1519c273c2eecf866c847d64e16baf
Hatice <haticekaraty@gmail.com>
2021-10-27 14:19:33 -0400

    update custom css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React from "react";
 import {Switch, Route} from "react-router-dom"
-import { connect } from 'react-redux'
-import {autoLogin} from "./actions/userActions"
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Navbar from "./components/NavBar";
@@ -11,22 +9,18 @@ import Footer from './components/Footer';
 import NotFound from './components/NotFound';
 
 class App extends React.Component{
- 
 
   render(){
-    //  this.props.autoLogin()
-    console.log("current user:",this.props.currentUser)
-    console.log("is logged in?:",this.props.loggedIn)
       return (
         <>
-        <div className="app-custom font-custom content-container">
+        <div className="app-custom">
           <Navbar />
-          <Switch>
-            <Route exact path="/budgets" component={(routeInfo)=> <BudgetsContainer routeInfo={routeInfo} />} />
-            <Route exact path="/" component={(routeInfo)=> <Login routeInfo={routeInfo} />} />
-            <Route exact path="/signup" component={(routeInfo)=> <SignUp routeInfo={routeInfo} />} />
-            <Route component= {NotFound} />
-          </Switch>
+            <Switch>
+              <Route exact path="/budgets" component={(routeInfo)=> <BudgetsContainer routeInfo={routeInfo} />} />
+              <Route exact path="/" component={(routeInfo)=> <Login routeInfo={routeInfo} />} />
+              <Route exact path="/signup" component={(routeInfo)=> <SignUp routeInfo={routeInfo} />} />
+              <Route component= {NotFound} />
+            </Switch>
         </div>
         <Footer />
         </>
@@ -34,12 +28,4 @@ class App extends React.Component{
   }
 
 }
-
-const mapStateToProps = (state) =>{
-    return {
-      currentUser : state.users.currentUser,
-      loggedIn : state.users.loggedIn
-    }
-
-}
-export default connect(mapStateToProps,{autoLogin})(App);
+export default (App);
